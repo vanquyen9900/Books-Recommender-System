97ac6b5ca1c26e5766f430956cb70bdd10e23a1e
Nguyễn Văn Quyền <vanquyen9900@gmail.com>
2026-04-19 13:26:17 +0700

    utils, exception and logger added

---
+++ app.py
@@ -0,0 +1,13 @@
+from books_recommender.exception.exception_handler import AppException
+
+import sys
+
+from books_recommender.logger.log import logging
+
+try:
+
+    a = 3/0
+
+except Exception as e:
+    logging.info(e)
+    raise AppException(e,sys) from e
